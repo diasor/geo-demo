ba1365510970646a6eb0fe6436109b739d2c4f77
Diana Sormani <diana@Dianas-MacBook-Air.local>
2020-12-29 08:01:52 +0100

    Change color palette.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,7 +8,7 @@ const vuetify = new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: colors.purple,
+		primary: "#6A1B9A",
         secondary: colors.grey.darken1,
         accent: colors.shades.black,
         error: colors.red.accent3,
